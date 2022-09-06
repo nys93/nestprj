8c8406172d96b1d448d8332a2c4e149561226be2
nys93 <dbthd0220@naver.com>
2022-09-07 03:31:47 +0900

    edit docker env and flyway sql

--- flyway/db-migration/V1__Initial.sql
+++ flyway/db-migration/V1__Initial.sql
@@ -1,25 +1,30 @@
-CREATE TABLE articles
-(
-    `article_id`    INT              NOT NULL    AUTO_INCREMENT COMMENT '게시글ID', 
-    `writer`      VARCHAR(50)      NOT NULL    COMMENT '작성자', 
-    `title`       VARCHAR(50)      NOT NULL    COMMENT '제목', 
-    `content`     VARCHAR(1000)    NOT NULL    COMMENT '내용', 
-    `regist_date`     DATETIME         NOT NULL    COMMENT '등록일자', 
-    `update_date`  DATETIME         NULL        COMMENT '수정일자', 
-    `delete_date`  DATETIME         NULL        COMMENT '삭제일자', 
-    PRIMARY KEY (article_id)
+DROP TABLE IF EXISTS articles;
+CREATE TABLE articles (
+  id int(11) NOT NULL AUTO_INCREMENT COMMENT '記事ID',
+  writer varchar(255) NOT NULL COMMENT '作成者',
+  title varchar(255) NOT NULL COMMENT 'タイトル',
+  content varchar(255) NOT NULL COMMENT '内容',
+  regist_date date NOT NULL COMMENT '登録日',
+  update_date date NOT NULL COMMENT '更新日',
+  delete_date date NOT NULL COMMENT '削除日',
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE replies
-(
-    `article_id`  INT              NOT NULL    COMMENT '게시글ID', 
-    `reply_id`  INT              NOT NULL    AUTO_INCREMENT COMMENT '댓글ID', 
-    `writer`    VARCHAR(50)      NOT NULL    COMMENT '작성자', 
-    `content`   VARCHAR(1000)    NOT NULL    COMMENT '내용', 
-    `regist_date`   DATETIME         NOT NULL    COMMENT '등록일자', 
-    PRIMARY KEY (reply_id)
+
+DROP TABLE IF EXISTS replies;
+CREATE TABLE replies (
+  id int(11) NOT NULL AUTO_INCREMENT COMMENT 'コメントID',
+  article_id int(11) DEFAULT NULL COMMENT '記事ID',
+  writer varchar(255) NOT NULL COMMENT '作成者',
+  content varchar(255) NOT NULL COMMENT '内容',
+  regist_date date NOT NULL COMMENT '登録日',
+  PRIMARY KEY (id),
+  CONSTRAINT fk_reply_article_id FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE ON UPDATE RESTRICT
 );
 
-ALTER TABLE replies
-    ADD CONSTRAINT reply_id_article_id FOREIGN KEY (article_id)
-        REFERENCES articles (article_id) ON DELETE CASCADE ON UPDATE RESTRICT;
+INSERT INTO articles (id, writer, title, content, regist_date, update_date, delete_date) VALUES
+(1, 'SONG', 'THIS IS AN ARTICLE TITLE', 'THIS IS AN ARTICLE CONTENT', '2022-09-06', '2022-09-06', '2022-09-06');
+
+INSERT INTO replies (id, article_id, writer, content, regist_date) VALUES
+(1, 1, 'CAT1', 'MEOW ARTICLE1', '2022-09-06'),
+(2, 1, 'CAT2', 'MEOW2 ARTICLE1', '2022-09-06');
